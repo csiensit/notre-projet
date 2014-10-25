e5df5b000e9c71672644b50184e69f10c2924138
csiensit@gmail.com <csiensit@gmail.com>
2014-10-25 03:29:09 +0100

    1

---
+++ aderant.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Sam 25 Octobre 2014 à 04:23
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `bd-csiensit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `aderant`
+--
+
+CREATE TABLE IF NOT EXISTS `aderant` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `pernom` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `filiaire` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
